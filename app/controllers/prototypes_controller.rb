e7bafac6648ed7a648945ef0d633ae7359576b9c
protein0917 <80035163+protein0917@users.noreply.github.com>
2021-04-01 19:15:35 +0900

    アクション名の変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -51,7 +51,7 @@ class PrototypesController < ApplicationController
 
   def move_to_index
     unless user_signed_in?
-      redirect_to action: :edit
+      redirect_to action: :index
     end
   end
 
